506a5db27d3d5330293404107019e4002d853143
Felipe Babel <Felipe.Babel@philips.com>
2024-02-25 00:32:22 -0300

    fix: fix changes 2

--- script.js
+++ script.js
@@ -11,14 +11,20 @@ function scrollToElement(elementSelector, instance = 0) {
 const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
+const link4 = document.getElementById("link4");
 
-link1.addEventListener('click', () => {
+
+link3.addEventListener('click', () => {
   scrollToElement('.header');
 });
 
 link2.addEventListener('click', () => {
   // Scroll to the second element with "header" class
   scrollToElement('.header', 1);
+});
+
+link3.addEventListener('click', () => {
+  scrollToElement('.column');
 });
 
 link3.addEventListener('click', () => {
