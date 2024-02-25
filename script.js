d60eeb35db62104451450a7f89d6ecd3c146658d
Felipe Babel <Felipe.Babel@philips.com>
2024-02-25 10:15:19 -0300

    fix: fix changes 5

--- script.js
+++ script.js
@@ -1,9 +1,6 @@
 function scrollToElement(elementSelector, instance = 0) {
-  // Select all elements that match the given selector
   const elements = document.querySelectorAll(elementSelector);
-  // Check if there are elements matching the selector and if the requested instance exists
   if (elements.length > instance) {
-      // Scroll to the specified instance of the element
       elements[instance].scrollIntoView({ behavior: 'smooth' });
   }
 }
@@ -12,21 +9,27 @@ const link1 = document.getElementById("link1");
 const link2 = document.getElementById("link2");
 const link3 = document.getElementById("link3");
 const link4 = document.getElementById("link4");
+const link5 = document.getElementById("link5");
 
-
-link3.addEventListener('click', () => {
-  scrollToElement('.header');
+link1.addEventListener('click', () => {
+  scrollToElement('.home');
 });
+
+
 
 link2.addEventListener('click', () => {
   // Scroll to the second element with "header" class
-  scrollToElement('.header', 1);
+  scrollToElement('.header');
 });
 
 link3.addEventListener('click', () => {
-  scrollToElement('.column');
+  scrollToElement('.header', 1);
 });
 
 link4.addEventListener('click', () => {
   scrollToElement('.header', 2);
+});
+
+link5.addEventListener('click', () => {
+  scrollToElement('.header', 3);
 });
